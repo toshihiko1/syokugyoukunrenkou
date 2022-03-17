6642659ac9421033b4ed06f1fdb99fc784b9bf78
toshihiko <greeeen8111@yahoo.co.jp>
2022-03-17 15:59:04 +0900

    20220317

---
+++ workspace/C/20220317_kurikaeshi_1.c
@@ -0,0 +1,133 @@
+#include <stdio.h>
+int main(void) {
+
+// 練習問題１
+// int a = 0;
+// for(int i = 0; i < 100; i++) {
+//     a += 1;
+//     printf("a=%d\n",a);
+// }
+
+// 2
+// int n = 0;
+// for(int i = 0;  i < 20; i++) {
+//     n += 1;
+//     printf("n=%d\n",n);
+// }
+
+// 3
+// for(int i = 0; i < k; i++){
+// }
+
+// 4
+// for(int n = 5; n <= 12; n++) {
+//     printf("n=%d\n",n);
+// }
+
+// 5
+// for(int n = 10; n >= 0; n--) {
+// }
+
+// 6
+// int wa = 0;
+// for(int i = 1; i <= 100; i++) {
+//     wa += i;
+// }
+// printf("wa=%d\n",wa);
+
+// 1
+// for(int i = 100; i >= 1; 100 % i) {
+//     int amari = i;
+//     if(amari != 0) {
+//         printf("100は%dで割り切れる\n",i);
+//         i = i - 1;
+//     }else{
+//     }
+// }
+
+// 2
+// int i = 100;
+// int amari;
+// while(i >= 1) {
+//     amari = 100 % i;
+//     if(amari == 0) {
+//         printf("100は%dで割り切れる\n",i);
+//     }
+//     i = i - 1;
+// }
+
+// 3
+// int wa = 0;
+// int data;
+// printf("整数値を入力");
+// scanf("%d",&data);
+// while( data != 0) {
+//     wa = wa + data;
+//     printf("%d\n",wa);
+//     printf("整数値を入力\n");
+//     scanf("%d",&data);
+// }
+
+
+// for(int wa = 0; data != 1; scanf("%d",&data)) {
+//     int amari = i;
+//     if(amari != 0) {
+//         printf("100は%dで割り切れる\n",i);
+//         i = i - 1;
+//     }else{
+//     }
+// }
+
+// 練習問題1
+// for(int i = 1000; i >= 0; i--) {
+//     if(i == 555) {
+//         printf("%d\n",i);
+//         break;
+//     }
+// }
+
+// 2
+// int input = 0;
+// int kekka = 0;
+// int count = 0;
+// printf("1〜100までの整数値を入力\n");
+// scanf("%d",&input);
+// while(count != input){
+//     count++;
+//     kekka += count;
+// }
+// printf("1〜%dまでの合計は%dです\n",input,kekka);
+
+// 2-2
+// int input = 0;
+// int total = 0;
+// printf("1〜100までの整数値を入力\n");
+// scanf("%d",&input);
+
+// for(int i = 1; i <= 100; i++) {
+//     total += i;
+//     if(i == input) {
+//         break;
+//     }
+// }
+// printf("1〜%dまでの合計は%dです\n",input,total);
+
+// 試してみよう無限ループ
+// double total = 0.0;
+// double weight;
+// while (1) {
+//     printf("小数値で体重を入力してください\n");
+//     scanf("%lf", &weight);
+//     if(weight == 0) {
+//         break;
+//         }else{
+//             total += weight;
+//     }
+// }
+// printf("体重の合計は%lfkgです\n", total);
+
+    
+
+
+return 0;
+}
---
+++ workspace/C/20220317_kurikaeshi_2.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+int main(void) {
+
+// int total = 0;
+// int money = 120;
+// int day =0;
+// while (1) {
+//     if(total >= 10000) {
+//         break;
+//     }else{
+//         total += money;
+//         day += 1;
+//     }
+// }
+// printf("%d日目で貯金額が%d円になりました\n",day,total);
+
+// int age;
+// while(1) {
+//     printf("年齢を入力してください\n");
+//     scanf("%d", &age);
+//     if(age == 0){
+//         break;
+//     }else if(age < 0 || age >= 100) {
+//         printf("エラー\n");
+//     }else if(age >= 20){
+//         printf("成年\n");
+//     }else if(age < 20) {
+//         printf("未成年\n");
+//     }
+// }
+
+// int pay = 0;
+// int ave = 0;
+// int sum = 0;
+// int count = 0;
+// while(1){
+//     printf("給料を入力してください\n");
+//     scanf("%d", &pay);
+//     if(pay == 0) {
+//         break;
+//     }
+//     count++;
+//     sum += pay;
+//     ave = sum / count;
+// }
+// ave = ave + 0.5;
+// printf("給料の合計%d円,給料の平均%d\n", sum,ave);
+
+// int strike = 0;
+// int ball = 0;
+// int count = 0;
+// while(1){
+//     printf("ストライクなら１、ボールなら２を入力\n");
+//     scanf("%d",&count);
+//     if(count == 1){
+//         strike++;
+//     }else{
+//         ball++;
+//     }  
+//     if(strike == 3 || ball == 4) {
+//         break;
+//     }
+// }
+// printf("%dストライク、%dボール\n",strike,ball);
+
+int strike = 0;
+int ball = 0;
+int count = 0;
+int foul = 0;
+while(1){
+    printf("ストライクなら１、ボールなら２を入力、ファウルなら３を入力\n");
+    scanf("%d",&count);
+    if(count == 1){
+        strike++;
+    }else if(count == 2){
+        ball++;
+    }else if(foul == 2) {
+    }else{
+        foul++;
+        strike++;
+    }
+    if(strike == 3 || ball == 4) {
+        break;
+    }
+}
+printf("%dストライク、%dボール\n",strike,ball);
+
+    return 0;
+}
